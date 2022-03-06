017aac817f228d8377934d3fca058ff51a9b5a28
yashdchintawar <yashchintawar31@gmail.com>
2022-03-06 15:29:59 +0530

    oprations added

--- src/com/basicStudy/Main.java
+++ src/com/basicStudy/Main.java
@@ -4,9 +4,12 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, I Am Yash");
+        System.out.println("Welcome To Yash Chintawar Code");
         int a = 20;
         int b = 10;
         System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
     }
 }
