1cbf9504f0c1dd51da732377013a6ece7ba7f914
yashdchintawar <yashchintawar31@gmail.com>
2022-03-06 15:22:24 +0530

    commit demo first

---
+++ src/com/basicStudy/Main.java
@@ -0,0 +1,12 @@
+package com.basicStudy;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, I Am Yash");
+        int a = 20;
+        int b = 10;
+        System.out.println(a+b);
+    }
+}
